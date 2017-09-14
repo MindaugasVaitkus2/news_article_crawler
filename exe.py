2fea936e661a2e1b93306172829d3dfd130dd451
asahi417 <spacefunkspacefunk@gmail.com>
2017-09-14 15:52:41 +0900

    bug

--- exe.py
+++ exe.py
@@ -2,27 +2,31 @@ from models import crawler, create_log, connectPSQL, ts
 from sqlalchemy.sql import select
 
 if __name__ == '__main__':
+
     target_cat = [1, 2, 3, 4, 5, 6, 7, 8]
-    max_page = 100
+    max_page = 5
     logger = create_log("./models/logger")
     db_inst = connectPSQL()
     con = db_inst.engine.connect()
     time = []
-    for t in target_cat:
-        s = select([ts.article_categories.update_time],
-                   ts.article_categories.category_ind == t)
-        r = con.execute(s)
-        time.append([r_ for r_ in r][0][0])
-        db_inst.session.query(ts.article_categories).filter(
-            ts.article_categories.category_ind == t).delete()
-        db_inst.session.commit()
+    try:
+        for t in target_cat:
+            s = select([ts.article_categories.update_time],
+                       ts.article_categories.category_ind == t)
+            r = con.execute(s)
+            time.append([r_ for r_ in r][0][0])
+            db_inst.session.query(ts.article_categories).filter(
+                ts.article_categories.category_ind == t).delete()
+            db_inst.session.commit()
 
-    df_art, df_time = crawler(logger, max_page=max_page, categories=target_cat,
-                              update_time=time)
+        df_art, df_time = crawler(logger, max_page=max_page, categories=target_cat,
+                                  update_time=time)
 
-    logger.info("inserting data shaped "+str(df_art.shape))
-    df_art.to_sql(ts.article_contents.__tablename__, db_inst.engine,
-                  if_exists='append', index=False)
-    logger.info("inserting time shaped "+str(df_time.shape))
-    df_time.to_sql(ts.article_categories.__tablename__, db_inst.engine,
-                   if_exists='append', index=False)
+        logger.info("inserting data shaped "+str(df_art.shape))
+        df_art.to_sql(ts.article_contents.__tablename__, db_inst.engine,
+                      if_exists='append', index=False)
+        logger.info("inserting time shaped "+str(df_time.shape))
+        df_time.to_sql(ts.article_categories.__tablename__, db_inst.engine,
+                       if_exists='append', index=False)
+    except Exception as err:
+        logger.error(err)
